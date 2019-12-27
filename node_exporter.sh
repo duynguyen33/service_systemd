3f0c5719628ef495b79518fafa8be6155932ea10
duynguyen33 <59276592+duynguyen33@users.noreply.github.com>
2019-12-28 01:25:53 +0700

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/usr/sbin/node_exporter --collector.tcpstat --collector.systemd --collector.mountstats --collector.buddyinfo --collector.interrupts --collector.ksmd --collector.logind --collector.processes --collector.meminfo_numa --collector.ntp --collector.supervisord
+cmd="/usr/sbin/node_exporter --collector.tcpstat --collector.mountstats --collector.buddyinfo --collector.interrupts --collector.ksmd --collector.logind --collector.processes --collector.meminfo_numa --collector.ntp"
+
+$cmd &
