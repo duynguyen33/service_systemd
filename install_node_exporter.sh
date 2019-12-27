9b96ef34ec203f3d0bfd806f9f6c3e817c7f2e34
duynguyen33 <59276592+duynguyen33@users.noreply.github.com>
2019-12-27 14:14:48 +0700

    Create install_node_exporter.sh

---
+++ install_node_exporter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+tar xzf node_exporter-0.18.1.linux-amd64.tar.gz
+cp node_exporter-0.18.1.linux-amd64/node_exporter /usr/sbin
+cp node-exporter.service /usr/lib/systemd/system
+mkdir /opt/monitor-prometheus
+mv node_exporter-0.18.1.linux-amd64.tar.gz /opt/monitor-prometheus
+rm -rf node_exporter-0.18.1.linux-*
+
+systemctl daemon-reload
+systemctl enable node-exporter
+systemctl start node-exporter
