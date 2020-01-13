4c9988eb6e1cd99064303cf80c1152717a51f76d
duynguyen33 <59276592+duynguyen33@users.noreply.github.com>
2020-01-13 15:20:44 +0700

    Update and rename install_node_exporter.sh to 02_install_node_exporter.sh

--- 02_install_node_exporter.sh
+++ 02_install_node_exporter.sh
@@ -2,6 +2,7 @@
 wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
 tar xzf node_exporter-0.18.1.linux-amd64.tar.gz
 cp node_exporter-0.18.1.linux-amd64/node_exporter /usr/sbin
+chmod a+x /usr/sbin/node_exporter
 cp node-exporter.service /usr/lib/systemd/system
 mkdir /opt/monitor-prometheus
 mv node_exporter-0.18.1.linux-amd64.tar.gz /opt/monitor-prometheus
