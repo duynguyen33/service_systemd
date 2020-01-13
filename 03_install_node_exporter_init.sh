f3d5909697fec58ff7c93127a9703cdc771cf755
duynguyen33 <kinglid.uit@gmail.com>
2020-01-13 15:33:06 +0700

    Add update

--- 03_install_node_exporter_init.sh
+++ 03_install_node_exporter_init.sh
@@ -9,7 +9,7 @@ cp node-exporter /etc/init.d/
 mv node_exporter-0.18.1.linux-amd64.tar.gz /opt/monitor-prometheus
 chmod +x /usr/sbin/node-exporter.sh
 chmod +x /opt/monitor-prometheus/node-exporter.sh
-chmod +x /etc/init.d/node_exporter
+chmod +x /etc/init.d/node-exporter
 chkconfig --add node-exporter
 rm -rf node_exporter-0.18.1.linux-*
 service node-exporter start
