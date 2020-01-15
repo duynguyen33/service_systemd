e945f8b5b746bbb9ae2af4309f9c2f52d4e7ad9f
duynguyen33 <kinglid.uit@gmail.com>
2020-01-15 11:31:34 +0700

    Update config

--- nginx-exporter.sh
+++ nginx-exporter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cmd="/usr/sbin/nginx-prometheus-exporter -nginx.scrape-uri http://localhost:8080/status"
+cmd="/usr/sbin/nginx-prometheus-exporter -nginx.scrape-uri http://localhost/status"
 
 $cmd &
